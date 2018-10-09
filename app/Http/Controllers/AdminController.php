7b4b40ae6405838be21bd6bc47ac36574799090e
vagrant <vagrant@localhost.localdomain>
2018-10-09 03:40:09 +0100

    complete admin_order_detail

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -18,7 +18,7 @@ class AdminController extends Controller
         return view('/admin/index');
     }
 
-    /** 月別の商品をすべて取得 */
+    /** 月別商品情報取得 */
     public function show_earning(Request $request) {
 
         $selected_day = new Carbon($request->input('month'));
@@ -37,8 +37,23 @@ class AdminController extends Controller
         return view('/admin/earnings', compact('selected_year', 'selected_month', 'total_price', 'orders_in_month'));
     }
 
-    /** 選択した日付の注文番号と注文金額を取得 */
-    public function select_earning_data($selected_day) {
+    /** 注文詳細情報取得 */
+    public function show_earning_detail($id) {
+        $earning_detail_datas = Order::select('od.id as id', 'i.name as name', 'od.price as price', 'od.payment_number as number')
+            ->selectRaw('od.price * od.payment_number as total_price')
+            ->from('orders as o')
+            ->join('order_details as od', 'o.id', '=', 'od.order_id')
+            ->join('items as i', 'i.id', '=', 'od.item_id')
+            ->where('o.id', '=', $id)
+            ->get();
+
+        return view('/admin/earning_detail', compact('earning_detail_datas'));
+    }
+
+    /** private */
+
+    /** 選択日付の注文番号と注文金額を取得 */
+    private function select_earning_data($selected_day) {
 
         $selected_year = date_format($selected_day, 'Y');
         $selected_month = date_format($selected_day, 'm');
@@ -59,8 +74,8 @@ class AdminController extends Controller
         return $orders_in_month;
     }
 
-    /** CSV出力するデータの格納 */
-    public function csv_format($orders_in_month, $total_price) {
+    /** CSV出力データの格納 */
+    private function csv_format($orders_in_month, $total_price) {
         $row = [];
         $row[] = array('注文番号', '注文金額', '合計金額');
         foreach ($orders_in_month as $id => $order) {
@@ -75,7 +90,7 @@ class AdminController extends Controller
     }
 
     /** CSVダウンロード */
-    public function downloadCSV($csv) {
+    private function downloadCSV($csv) {
         return new StreamedResponse(
             function() use ($csv) {
                 $stream = fopen('php://output', 'w');
@@ -90,22 +105,6 @@ class AdminController extends Controller
                 'Content-Disposition' => 'attachment; filename="users.csv"',
             ]
         );
-    }
-
-    public function show_earning_detail($id) {
-        $earning_detail_datas = select(DB::raw(
-                'order_details.id as id, 
-                items.name as name, 
-                order_details.price as price, 
-                order_details.payment_number as number, 
-                order_details.price * order_details.payment_number as total_price'
-            ))
-            ->join('order_details', 'orders.id', '=', 'order_details.order_id')
-            ->join('items', 'items.id', '=', 'order_details.item_id')
-            ->where('orders.id', '=', $id)
-            ->get();
-
-        return view('/admin/earning_detail', compact('earning_detail_datas'));
     }
 
 }
